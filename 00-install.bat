08343be9f97b9b1ee52bfe6f7551bf345da48ba6
Daniel Chen <chendaniely@gmail.com>
2020-01-25 20:16:26 -0500

    english document with manual data entry to var

---
+++ 00-install.bat
@@ -0,0 +1,4 @@
+C:\"Program Files"\R\R-3.6.2\bin\Rscript.exe -e "install.packages(c('ggplot2', 'png', 'extrafont', 'knitr', 'rmarkdown', 'shiny', 'shinydashboard', 'tinytex'), repo = 'https://cloud.r-project.org/')"
+C:\"Program Files"\R\R-3.6.2\bin\Rscript.exe -e "tinytex::install_tinytex()"
+C:\"Program Files"\R\R-3.6.2\bin\Rscript.exe -e "tinytex:::install_yihui_pkgs()"
+@PAUSE
