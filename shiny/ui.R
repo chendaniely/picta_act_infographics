ed072d79b7c1bbc67bdbe20fecaf9caa944a48bd
Daniel Chen <chendaniely@gmail.com>
2020-02-01 02:31:52 -0800

    move things into dashboard sidebar menus

--- shiny/ui.R
+++ shiny/ui.R
@@ -2,43 +2,54 @@ library(shinydashboard)
 
 dashboardPage(
   dashboardHeader(title = "PICTA ACT"),
-  dashboardSidebar(disable = TRUE
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Single Image", tabName = "single_image", icon = icon("dice-one")),
+      menuItem("Batch File", tabName = "batch_file", icon = icon("table")),
+      menuItem("Debug", tabName = "debug", icon = icon("bug"))
+    )
   ),
   dashboardBody(
-    fluidRow(
-      box(
-        width = 4,
-        selectInput("language", label = h3("Language"), 
-                    choices = list("English" = "english", "Spanish" = "spanish"), 
-                    selected = "english"),
-        textInput("Name", label = h3("Name"), value = "Enter name ...")
+    tabItems(
+      # First tab content
+      tabItem(tabName = "single_image",
+              fluidRow(
+                box(
+                  width = 4,
+                  selectInput("language", label = h3("Language"), 
+                              choices = list("English" = "english", "Spanish" = "spanish"), 
+                              selected = "english"),
+                  textInput("Name", label = h3("Name"), value = "Enter name ...")
+                ),
+                
+                box(
+                  width = 4,
+                  dateInput("today_date", label = h3("Today's date"), value = Sys.Date()),
+                  numericInput("today_act", label = h3("Today ACT"), value = 25)
+                ),
+                box(
+                  width = 4,
+                  dateInput("previous_date", label = h3("Previous date"), value = Sys.Date()),
+                  numericInput("previous_act", label = h3("Previous ACT"), value = 5),
+                )
+              ),
+              fluidRow(
+                box(#height = "100%",
+                  width = 12,
+                  imageOutput("plot", width = "100%")
+                )
+                
+              )
       ),
       
-      box(
-        width = 4,
-        dateInput("today_date", label = h3("Today's date"), value = Sys.Date()),
-        numericInput("today_act", label = h3("Today ACT"), value = 25)
-      ),
-      box(
-        width = 4,
-        dateInput("previous_date", label = h3("Previous date"), value = Sys.Date()),
-        numericInput("previous_act", label = h3("Previous ACT"), value = 5),
+      # Second tab content
+      tabItem(tabName = "debug",
+              fluidRow(
+                box(title = "Debug Info",
+                    verbatimTextOutput("pt_list")
+                )
+              )
       )
-    ),
-    fluidRow(
-      box(#height = "100%",
-          width = 12,
-          imageOutput("plot", width = "100%")
-      )
-      
-    ),
-    fluidRow(
-      box(collapsed = TRUE,
-          collapsible = TRUE,
-          title = "Debug Info",
-          verbatimTextOutput("pt_list")
-      )
-      
     )
   )
 )
