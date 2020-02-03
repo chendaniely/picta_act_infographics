c41755244d5fbf0c2abd0ff1d0289331392ff0af
Daniel Chen <chendaniely@gmail.com>
2020-02-03 11:20:08 -0500

    move all R code into subdir

--- generate-pdf.bat
+++ generate-pdf.bat
@@ -1,2 +1,2 @@
-C:\"Program Files"\R\R-3.6.2\bin\Rscript.exe -e "knitr::knit2pdf(input = 'picta_act/documents/act-pamphlet_interrior-english.Rnw', output = 'picta_act/documents/pct.tex', compiler = 'xelatex')"
+C:\"Program Files"\R\R-3.6.2\bin\Rscript.exe -e "knitr::knit2pdf(input = 'src/picta_act/documents/act-pamphlet_interrior-english.Rnw', output = 'picta_act/documents/pct.tex', compiler = 'xelatex')"
 @PAUSE
