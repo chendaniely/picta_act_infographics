45562fcb341ac9c9e31c7d5e472829f2fb08613f
Daniel Chen <chendaniely@gmail.com>
2020-02-03 19:42:25 -0500

    spanish progress statements

--- pictaACTinfographics/shiny/server.R
+++ pictaACTinfographics/shiny/server.R
@@ -37,16 +37,32 @@ server <- function(input, output, session) {
 
   gen_asthma_progress_statment <- function(today_act_score, previous_act_score, language) {
     if (previous_act_score - today_act_score >= 3) {
-      return("It has gotten worse since your last visit.")
+      if (language == "spanish") {
+        return("Ha empeorado desde su última visita.")
+      } else {
+        return("It has gotten worse since your last visit.")
+      }
     } else if (today_act_score <= 19 && abs(previous_act_score - today_act_score) <= 2) {
+      if (language == "spanish") {
+        return("Sigue igual que desde su última visita.")
+      } else {
       return("It is about the same as at your last visit.")
+      }
     } else if (today_act_score >= 20 &&
                (today_act_score - previous_act_score >= 0 || previous_act_score - today_act_score >= 2)) {
       return("")
     } else if (today_act_score <= 19 && today_act_score - previous_act_score >= 3) {
+      if (language == "spanish") {
+        return("Ha habido mejora!")
+      } else {
       return("You made good progress!")
+      }
     } else if (today_act_score >= 20 && previous_act_score <= 19) {
-      return("Great job!")
+      if (language == "spanish") {
+        return("Muy bien!")
+      } else {
+        return("Great job!")
+      }
     } else {
       stop("Unknown progress statement condition")
     }
