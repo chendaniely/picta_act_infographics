1e4f3f925427124c19bcf7a8d941e4f45670aab4
Daniel Chen <chendaniely@users.noreply.github.com>
2020-02-14 09:48:20 -0500

    add remotes to install

--- 00-install.bat
+++ 00-install.bat
@@ -1,4 +1,4 @@
-C:\"Program Files"\R\R-3.6.2\bin\Rscript.exe -e "install.packages(c('ggplot2', 'png', 'extrafont', 'knitr', 'rmarkdown', 'shiny', 'shinydashboard', 'tinytex', 'here', 'RCurl', 'devtools'), repo = 'https://cloud.r-project.org/')"
+C:\"Program Files"\R\R-3.6.2\bin\Rscript.exe -e "install.packages(c('remotes', 'ggplot2', 'png', 'extrafont', 'knitr', 'rmarkdown', 'shiny', 'shinydashboard', 'tinytex', 'here', 'RCurl', 'devtools'), repo = 'https://cloud.r-project.org/')"
 C:\"Program Files"\R\R-3.6.2\bin\Rscript.exe -e "remotes::remotes::install_github('wilkelab/ggtext')"
 C:\"Program Files"\R\R-3.6.2\bin\Rscript.exe -e "tinytex::install_tinytex()"
 C:\"Program Files"\R\R-3.6.2\bin\Rscript.exe -e "tinytex:::install_yihui_pkgs()"
