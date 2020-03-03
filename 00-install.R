a341583ae7d1db48224a5a9dd507206599c82ece
Daniel Chen <chendaniely@gmail.com>
2020-03-03 03:23:23 -0500

    no longer need ggtext

--- 00-install.R
+++ 00-install.R
@@ -1,4 +1,4 @@
 install.packages(c('ggplot2', 'png', 'extrafont', 'knitr', 'rmarkdown', 'shiny', 'shinydashboard', 'tinytex', 'here', 'RCurl', 'devtools', 'remotes', 'readr', 'DT'), repo = 'https://cloud.r-project.org/')
-remotes::install_github('wilkelab/ggtext')
+#remotes::install_github('wilkelab/ggtext')
 tinytex::install_tinytex()
 tinytex:::install_yihui_pkgs()
