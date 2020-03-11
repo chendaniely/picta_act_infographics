ccc7b6dbaf5494534669ddf45a853969cd56c709
Daniel Chen <chendaniely@gmail.com>
2020-03-10 23:52:04 -0400

    clean up, move, label code sections

--- pictaACTinfographics/shiny/global.R
+++ pictaACTinfographics/shiny/global.R
@@ -5,6 +5,130 @@ library(shiny)
 library(readr)
 library(DT)
 
+theme_nothing_text <- function(base_size = 12, base_family = "Arial")
+{
+  theme_bw(base_size = base_size, base_family = base_family) %+replace%
+    theme(
+      rect              = element_blank(),
+      line              = element_blank(),
+      axis.ticks        = element_blank()
+    )
+}
+
+translate_eng_mo_to_sp <- function(eng_mo) {
+  eng_mo <- stringr::str_to_lower(eng_mo)
+  switch(eng_mo,
+         'january' = 'enero',
+         'february' = 'febrero',
+         'march' = 'marzo',
+         'april' = 'abril',
+         'may' = 'mayo',
+         'june' = 'junio',
+         'july' = 'julio',
+         'august' = 'agosto',
+         'september' = 'septiembre',
+         'october' = 'octubre',
+         'november' = 'noviembre',
+         'december' = 'diciembre',
+         stop(glue::glue("Unknown english month to translate into spanish: {eng_mo}"))
+  )
+}
+
+gen_x_coords <- function(language) {
+  if (language == "spanish") {
+    arrow_x_poor <- c( 8.7, 11.8, 14.9, 18.0, 20.7, # 5
+                       23.8, 26.9, 30.0, 33.1, 36.5, # 10
+                       40.4, 44.1, 47.8, 51.5, 55.2  # 15
+    )
+    
+    arrow_x_16 <- 59.5
+    arrow_x_notwell <- c(63.6, 67.4, 71.1)
+    arrow_x_20 <- 75
+    arrow_x_well <- c(79.4, 83.2, 86.9, 90.6)
+    arrow_x_25 <- 95.0
+  } else {
+    arrow_x_poor <- c( 8.7, 11.8, 14.9, 18.0, 21.1, # 5
+                       24.2, 27.3, 30.4, 33.5, 36.8, # 10
+                       40.5, 44.2, 47.9, 51.6, 55.3  # 15
+    )
+    
+    arrow_x_16 <- 59.5
+    arrow_x_notwell <- c(63.5, 67.2, 70.9)
+    arrow_x_20 <- 75
+    arrow_x_well <- c(79, 82.7, 86.4, 90.1)
+    arrow_x_25 <- 94.2
+  }
+  arrow_x_all <- c(arrow_x_poor, arrow_x_16, arrow_x_notwell, arrow_x_20, arrow_x_well, arrow_x_25)
+  return(arrow_x_all)
+}
+
+gen_asthma_date_blob <- function(date, language) {
+  if (language == "spanish") {
+    translated_mo <- translate_eng_mo_to_sp(strftime(date, "%B"))
+    format_eng <- strftime(date,
+                           glue::glue("Fecha:  %d de {translated_mo}, %Y"))
+  } else {
+    return(strftime(date, "Date:  %B %d, %Y"))
+  }
+}
+
+gen_asthma_interpretive_statement_blob <- function(today_act_score, language) {
+  if (today_act_score %in% 5:15) {
+    if (language == "spanish") {
+      return("Su asma está muy mal controlada")
+    } else {
+      return("Your asthma is very poorly controlled")
+    }
+  } else if (today_act_score %in% 16:19) {
+    if (language == "spanish") {
+      return("Su asma está mal controlada")
+    } else {
+      return("Your asthma is not well controlled")
+    }
+  } else if (today_act_score %in% 20:25) {
+    if (language == "spanish") {
+      return("Su asma está bien controlada")
+    } else {
+      return("Your asthma is well controlled")
+    }
+  } else {
+    stop(sprintf("Invalid act score given. Expected 5-25, got %s", today_act_score))
+  }
+}
+
+gen_asthma_progress_statment <- function(today_act_score, previous_act_score, language) {
+  if (previous_act_score - today_act_score >= 3) {
+    if (language == "spanish") {
+      return("Ha empeorado desde su última visita.")
+    } else {
+      return("It has gotten worse since your last visit.")
+    }
+  } else if (today_act_score <= 19 && abs(previous_act_score - today_act_score) <= 2) {
+    if (language == "spanish") {
+      return("Sigue igual que desde su última visita.")
+    } else {
+      return("It is about the same as at your last visit.")
+    }
+  } else if (today_act_score >= 20 &&
+             (today_act_score - previous_act_score >= 0 || previous_act_score - today_act_score >= 2)) {
+    return("")
+  } else if (today_act_score <= 19 && today_act_score - previous_act_score >= 3) {
+    if (language == "spanish") {
+      return("Ha habido mejora!")
+    } else {
+      return("You made good progress!")
+    }
+  } else if (today_act_score >= 20 && previous_act_score <= 19) {
+    if (language == "spanish") {
+      return("Muy bien!")
+    } else {
+      return("Great job!")
+    }
+  } else {
+    stop("Unknown progress statement condition")
+  }
+}
+
 dummy <- data.frame(x = c(0,100), y = c(0, 100)) # need this to actually show plot
 
 score_arrow_y1 = 38
--- pictaACTinfographics/shiny/server.R
+++ pictaACTinfographics/shiny/server.R
@@ -1,130 +1,7 @@
 
 server <- function(input, output, session) {
-  
-  theme_nothing_text <- function(base_size = 12, base_family = "Arial")
-  {
-    theme_bw(base_size = base_size, base_family = base_family) %+replace%
-      theme(
-        rect              = element_blank(),
-        line              = element_blank(),
-        axis.ticks        = element_blank()
-      )
-  }
 
-  translate_eng_mo_to_sp <- function(eng_mo) {
-    eng_mo <- stringr::str_to_lower(eng_mo)
-    switch(eng_mo,
-           'january' = 'enero',
-           'february' = 'febrero',
-           'march' = 'marzo',
-           'april' = 'abril',
-           'may' = 'mayo',
-           'june' = 'junio',
-           'july' = 'julio',
-           'august' = 'agosto',
-           'september' = 'septiembre',
-           'october' = 'octubre',
-           'november' = 'noviembre',
-           'december' = 'diciembre',
-           stop(glue::glue("Unknown english month to translate into spanish: {eng_mo}"))
-    )
-  }
-
-  gen_x_coords <- function(language) {
-    if (language == "spanish") {
-      arrow_x_poor <- c( 8.7, 11.8, 14.9, 18.0, 20.7, # 5
-                   23.8, 26.9, 30.0, 33.1, 36.5, # 10
-                   40.4, 44.1, 47.8, 51.5, 55.2  # 15
-                   )
-
-      arrow_x_16 <- 59.5
-      arrow_x_notwell <- c(63.6, 67.4, 71.1)
-      arrow_x_20 <- 75
-      arrow_x_well <- c(79.4, 83.2, 86.9, 90.6)
-      arrow_x_25 <- 95.0
-    } else {
-      arrow_x_poor <- c( 8.7, 11.8, 14.9, 18.0, 21.1, # 5
-                   24.2, 27.3, 30.4, 33.5, 36.8, # 10
-                   40.5, 44.2, 47.9, 51.6, 55.3  # 15
-                   )
-
-      arrow_x_16 <- 59.5
-      arrow_x_notwell <- c(63.5, 67.2, 70.9)
-      arrow_x_20 <- 75
-      arrow_x_well <- c(79, 82.7, 86.4, 90.1)
-      arrow_x_25 <- 94.2
-    }
-    arrow_x_all <- c(arrow_x_poor, arrow_x_16, arrow_x_notwell, arrow_x_20, arrow_x_well, arrow_x_25)
-    return(arrow_x_all)
-  }
-
-  gen_asthma_date_blob <- function(date, language) {
-    if (language == "spanish") {
-      translated_mo <- translate_eng_mo_to_sp(strftime(date, "%B"))
-      format_eng <- strftime(date,
-                             glue::glue("Fecha:  %d de {translated_mo}, %Y"))
-    } else {
-      return(strftime(date, "Date:  %B %d, %Y"))
-    }
-  }
-  
-  gen_asthma_interpretive_statement_blob <- function(today_act_score, language) {
-    if (today_act_score %in% 5:15) {
-      if (language == "spanish") {
-        return("Su asma está muy mal controlada")
-      } else {
-        return("Your asthma is very poorly controlled")
-      }
-    } else if (today_act_score %in% 16:19) {
-      if (language == "spanish") {
-        return("Su asma está mal controlada")
-      } else {
-        return("Your asthma is not well controlled")
-      }
-    } else if (today_act_score %in% 20:25) {
-      if (language == "spanish") {
-        return("Su asma está bien controlada")
-      } else {
-        return("Your asthma is well controlled")
-      }
-    } else {
-      stop(sprintf("Invalid act score given. Expected 5-25, got %s", today_act_score))
-    }
-  }
-
-  gen_asthma_progress_statment <- function(today_act_score, previous_act_score, language) {
-    if (previous_act_score - today_act_score >= 3) {
-      if (language == "spanish") {
-        return("Ha empeorado desde su última visita.")
-      } else {
-        return("It has gotten worse since your last visit.")
-      }
-    } else if (today_act_score <= 19 && abs(previous_act_score - today_act_score) <= 2) {
-      if (language == "spanish") {
-        return("Sigue igual que desde su última visita.")
-      } else {
-      return("It is about the same as at your last visit.")
-      }
-    } else if (today_act_score >= 20 &&
-               (today_act_score - previous_act_score >= 0 || previous_act_score - today_act_score >= 2)) {
-      return("")
-    } else if (today_act_score <= 19 && today_act_score - previous_act_score >= 3) {
-      if (language == "spanish") {
-        return("Ha habido mejora!")
-      } else {
-      return("You made good progress!")
-      }
-    } else if (today_act_score >= 20 && previous_act_score <= 19) {
-      if (language == "spanish") {
-        return("Muy bien!")
-      } else {
-        return("Great job!")
-      }
-    } else {
-      stop("Unknown progress statement condition")
-    }
-  }
-  
+  # reactive functions -----
   today_score_arrow <- function() {
     geom_segment(aes(x = arrow_x_all()[PT_INFO()$today_act],
                      y = score_arrow_y1,
@@ -203,6 +80,7 @@ server <- function(input, output, session) {
                  arrow = arrow(length = unit(previous_score_arrow_length_unit, "cm")))
   }
   
+  # PT_INFO -----
   PT_INFO <- reactive({
     PT_VALUES_ASTHMA <- list(
       language = input$language,
@@ -248,23 +126,16 @@ server <- function(input, output, session) {
     PT_VALUES_ASTHMA
   })
   
+  # x coord for arrows -----
   arrow_x_all <- reactive({
     gen_x_coords(PT_INFO()$language)
   })
 
+  # base image -----
   image <- reactive({png::readPNG(PT_INFO()$png_url)})
   base_image_g <- reactive({grid::rasterGrob(image(), interpolate=TRUE)})
   
-  output$asthma_statements <- renderPrint({
-    print(glue::glue("Date text: {PT_INFO()$today_date_text}"))
-    print(glue::glue("Score: {PT_INFO()$asthma_score_statement}"))
-    print(glue::glue("Interpretive: {PT_INFO()$asthma_interpretive_statement}"))
-    print(glue::glue("Progress: {PT_INFO()$asthma_progress_statment}"))
-  })
   
-  output$pt_list <- renderPrint({PT_INFO()})
-  
-  output$cwd <- renderPrint({getwd()})
   
   plot_pth_norm <- reactive({fs::path_norm(tempfile(fileext = '.png'))})
   plot_pth_unix <- reactive({gsub("\\\\", "/", plot_pth_norm())})
@@ -275,12 +146,8 @@ server <- function(input, output, session) {
     ))
   })
   
-  output$plot_pth_debug <- renderPrint({
-    print(plot_pth_norm());
-    print(plot_pth_unix());
-    print(fig_pth_act_exterior())
-  })
-  
+
+  # image -----
   output$plot <- renderImage({
     outfile <- plot_pth_norm()
     
@@ -335,15 +202,18 @@ server <- function(input, output, session) {
   }, deleteFile = FALSE
   )
 
+  # pdf file name -----
   pdf_single_filename <- reactive({
     name <- gsub(" ", "_", input$name)
     print(name)
     return(glue::glue("act-{PT_INFO()$language}-{name}-{PT_INFO()$today_date}.pdf"))
   })
   
+  # single download button -----
   output$download_single <- downloadHandler(
-    filename = function() {pdf_single_filename()},
-    
+    filename = function() {
+      pdf_single_filename()
+    },
     content = function(file) {
       out = knitr::knit2pdf(input = PT_INFO()$act_rnw_f,
                             #output = glue::glue("{input$name}-{input$today_date}.tex"),
@@ -352,24 +222,12 @@ server <- function(input, output, session) {
                             compiler = "xelatex")
       file.rename(out, file) # move pdf to file for downloading
     },
-    
     contentType = 'application/pdf'
   )
-  
-  output$tinytex_info <- renderPrint({
-    print(tinytex:::is_tinytex())
-    print(tinytex::tinytex_root())
-  })
 
-  gen_pdf_filename <- function(language, name, date) {
-    return(glue::glue("act-{language}-{name}-{date}.pdf"))
-  }
 
-  output$pdf_single_fn <- renderPrint({print(pdf_single_filename())})
-  
   # batch file ----
   input_file <- reactive({input$file})
-  output$batch_file_pth <- renderPrint({str(input_file())})
  
   input_file_df <- reactive({
     if (!is.null(input_file())) {
@@ -420,4 +278,31 @@ server <- function(input, output, session) {
     },
     contentType = 'application/zip'
   )
+  
+  # debug print output -----
+  output$plot_pth_debug <- renderPrint({
+    print(plot_pth_norm());
+    print(plot_pth_unix());
+    print(fig_pth_act_exterior())
+  })
+  
+  output$asthma_statements <- renderPrint({
+    print(glue::glue("Date text: {PT_INFO()$today_date_text}"))
+    print(glue::glue("Score: {PT_INFO()$asthma_score_statement}"))
+    print(glue::glue("Interpretive: {PT_INFO()$asthma_interpretive_statement}"))
+    print(glue::glue("Progress: {PT_INFO()$asthma_progress_statment}"))
+  })
+  
+  output$pt_list <- renderPrint({PT_INFO()})
+  
+  output$cwd <- renderPrint({getwd()})
+  
+  output$tinytex_info <- renderPrint({
+    print(tinytex:::is_tinytex())
+    print(tinytex::tinytex_root())
+  })
+  
+  output$pdf_single_fn <- renderPrint({print(pdf_single_filename())})
+  
+  output$batch_file_pth <- renderPrint({str(input_file())})
 }
