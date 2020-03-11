9b04b97937d543ce1f753c90bd9a98ab1e4f8ba7
Daniel Chen <chendaniely@gmail.com>
2020-03-11 17:44:53 -0400

    add more install packages

--- 00-install/00-install.R
+++ 00-install/00-install.R
@@ -1,3 +1,3 @@
-install.packages(c('ggplot2', 'png', 'extrafont', 'knitr', 'rmarkdown', 'shiny', 'shinydashboard', 'tinytex', 'here', 'RCurl', 'devtools', 'remotes', 'readr', 'DT'), repo = 'https://cloud.r-project.org/')
+install.packages(c('ggplot2', 'png', 'extrafont', 'knitr', 'rmarkdown', 'shiny', 'shinydashboard', 'tinytex', 'here', 'RCurl', 'devtools', 'remotes', 'readr', 'DT', 'stringr', 'fs', 'lubridate', 'dplyr', 'purrr', 'janitor'), repo = 'https://cloud.r-project.org/')
 tinytex::install_tinytex()
 tinytex:::install_yihui_pkgs()
