e125579310c385b922d667da7c086174400f4e7e
Daniel Chen <chendaniely@users.noreply.github.com>
2020-03-11 12:51:41 -0400

    update RScript.exe location to Adriana

--- runApp.bat
+++ runApp.bat
@@ -1,2 +1,2 @@
-"Rscript.exe - Shortcut.lnk" -e "getwd(); shiny::runApp('pictaACTinfographics\\pictaACTinfographics\\shiny', launch.browser = TRUE)"
+"C:\Program Files\R\R-3.6.2\bin\x64\RScript.exe" -e "getwd(); shiny::runApp('pictaACTinfographics\\shiny', launch.browser = TRUE)"
 @PAUSE
