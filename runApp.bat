266fab6e2b4be9dd6f895cd191d6c9e712d87684
Daniel Chen <chendaniely@users.noreply.github.com>
2020-09-04 01:59:50 -0400

    Update runApp.bat

--- runApp.bat
+++ runApp.bat
@@ -1,2 +1,2 @@
-"C:\Program Files\R\R-3.6.2\bin\x64\RScript.exe" -e "getwd(); shiny::runApp('pictaACTinfographics\\shiny', launch.browser = TRUE)"
+"C:\Program Files\R\R-4.0.2\bin\x64\RScript.exe" -e "getwd(); shiny::runApp('pictaACTinfographics\\shiny', launch.browser = TRUE)"
 @PAUSE
